072f3afba683857b5ad5df7ced7a44c4902a102b
olakayCoder1 <programmerolakay@gmail.com>
2024-05-18 08:53:51 +0100

    Serialiser added to url add endpoint

--- src/main/java/com/olakayurlshot/urlshortener/controller/UrlShotController.java
+++ src/main/java/com/olakayurlshot/urlshortener/controller/UrlShotController.java
@@ -1,12 +1,16 @@
 package com.olakayurlshot.urlshortener.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,7 @@ import com.olakayurlshot.urlshortener.entity.User;
 import com.olakayurlshot.urlshortener.repository.UserRepository;
 import com.olakayurlshot.urlshortener.service.UrlShotService;
 
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,17 +43,32 @@ public class UrlShotController {
     @Autowired
     private UserRepository userRepository;
 
+    @SuppressWarnings("rawtypes")
     @PostMapping("/")
     @PreAuthorize(value = "hasRole('USER')")
-    public String addUrl(
-        @RequestBody UrlShot urlShot, 
+    public ResponseEntity addUrl(
+        @RequestBody @Valid UrlDTO entity, BindingResult bindingResult,
         @AuthenticationPrincipal UserDetails currentUser
         ) {
-        String username = currentUser.getUsername();
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));
-        urlShot.setUser(user);
-        urlShotService.addUrl(urlShot);
-        return "success";
+            if (bindingResult.hasErrors()) {
+                // Convert errors to a more readable format
+                Map<String, Object> errors = new HashMap<>();
+                errors.put("errors", bindingResult.getFieldErrors().stream()
+                        .collect(Collectors.toMap(
+                                error -> error.getField(),
+                                error -> error.getDefaultMessage()
+                        )));
+                return ResponseEntity.badRequest().body(errors);
+            }
+            String username = currentUser.getUsername();
+            User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));
+            UrlShot urlShot = new UrlShot();
+            urlShot.setUser(user);
+            urlShot.setName(entity.getName());;
+            urlShot.setUrl(entity.getUrl());
+            urlShot.setDescription(entity.getDescription());
+            urlShotService.addUrl(urlShot);
+        return ResponseEntity.ok().body(urlShot);
     }
  
     @GetMapping("/")
--- src/main/java/com/olakayurlshot/urlshortener/dto/UrlDTO.java
+++ src/main/java/com/olakayurlshot/urlshortener/dto/UrlDTO.java
@@ -1,9 +1,26 @@
 package com.olakayurlshot.urlshortener.dto;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UrlDTO {
     
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
     private String name;
+
+    @NotNull( message = "Name cannot be null")
+    private String url;
+
+    private String description;
+
 }
