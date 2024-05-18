4cb695822aba1eca10b9a634d7512e477418bdfd
olakayCoder1 <programmerolakay@gmail.com>
2024-05-18 08:54:23 +0100

    Serialiser added to url add endpoint

--- src/main/java/com/olakayurlshot/urlshortener/dto/UrlDTO.java
+++ src/main/java/com/olakayurlshot/urlshortener/dto/UrlDTO.java
@@ -1,7 +1,6 @@
 package com.olakayurlshot.urlshortener.dto;
 
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
